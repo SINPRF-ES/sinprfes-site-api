1522486b28c30e1fbdcc5418a601c8f4e5e1ec9f
mfavero138 <marcelo.mfb@gmail.com>
2026-01-08 12:08:04 -0300

    Update filiados-admin.js

--- public/js/area-filiado/filiados-admin.js
+++ public/js/area-filiado/filiados-admin.js
@@ -1,4 +1,12 @@
-import { apiFetch, aplicarMascaraTelefone, formatarCPF, normalizarTextoBusca, formatarTelefoneTexto } from './utils.js';
+import {
+  apiFetch,
+  aplicarMascaraTelefone,
+  formatarCPF,
+  normalizarTextoBusca,
+  formatarTelefoneTexto,
+  aplicarMascaraCPF,
+  aplicarMascaraCEP
+} from './utils.js';
 
 let cacheLista = [];
 const SITUACAO_OPCOES = ["ATIVO", "VETERANO", "PENSIONISTA"];
