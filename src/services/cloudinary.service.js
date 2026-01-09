f3534eb7641acc280c1f326d1b082212b8b17df8
mfavero138 <marcelo.mfb@gmail.com>
2026-01-08 23:20:12 -0300

    Update cloudinary.service.js

--- src/services/cloudinary.service.js
+++ src/services/cloudinary.service.js
@@ -2,11 +2,7 @@
 const cloudinary = require("cloudinary").v2;
 const streamifier = require("streamifier");
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+cloudinary.config(process.env.CLOUDINARY_URL);
 
 /**
  * Upload de avatar com:
