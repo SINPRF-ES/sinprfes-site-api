9c03ddb8be33df012752de632d8e18384bc515e4
mfavero138 <marcelo.mfb@gmail.com>
2026-01-28 17:56:22 -0300

    Update HomeScreen.tsx (retirada do OTA teste)

--- mobile/src/screens/HomeScreen.tsx
+++ mobile/src/screens/HomeScreen.tsx
@@ -47,7 +47,6 @@ export default function HomeScreen({ navigation }: HomeScreenProps) {
         {usuario?.situacao && (
           <View style={styles.statusBadge}>
             <Text style={styles.statusText}>{usuario.situacao}</Text>
-            <Text>OTA OK</Text>
           </View>
         )}
       </View>
