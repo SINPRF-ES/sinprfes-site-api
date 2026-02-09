16473ab9df58212bc7cda1a3f628a7d30299b11b
mfavero138 <marcelo.mfb@gmail.com>
2026-02-09 09:25:14 -0300

    Update App.tsx

--- FENAPRF/mobile/App.tsx
+++ FENAPRF/mobile/App.tsx
@@ -1,20 +1,20 @@
-import React, { useEffect } from 'react';
-import Constants from 'expo-constants';
-import ReanimatedPackage from 'react-native-reanimated/package.json';
-import { AuthProvider } from './src/hooks/useAuth';
-import RootNavigation from './src/navigation';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { initDb } from './src/database/db';
-import { setupGlobalErrorHandling } from './src/infra/errorHandling';
-import ErrorBoundary from './src/components/ErrorBoundary';
-import { logger } from './src/infra/logger';
-import UpdateAutoChecker from './src/components/UpdateAutoChecker';
+// App.tsx
+import React, { useEffect } from "react";
+import Constants from "expo-constants";
+import ReanimatedPackage from "react-native-reanimated/package.json";
+import { AuthProvider } from "./src/hooks/useAuth";
+import RootNavigation from "./src/navigation";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { initDb } from "./src/database/db";
+import { setupGlobalErrorHandling } from "./src/infra/errorHandling";
+import ErrorBoundary from "./src/components/ErrorBoundary";
+import { logger } from "./src/infra/logger";
 
 // Inicializa o sistema de captura de erros globalmente
 setupGlobalErrorHandling();
 
 // Log de inicialização com versões importantes
-logger.info('App Initializing', {
+logger.info("App Initializing", {
   expoSdkVersion: Constants.expoVersion,
   reanimatedVersion: ReanimatedPackage.version,
 });
@@ -23,7 +23,7 @@ const queryClient = new QueryClient();
 
 export default function App() {
   useEffect(() => {
-    initDb().catch((e) => console.warn('Erro ao inicializar DB:', e));
+    initDb().catch((e) => console.warn("Erro ao inicializar DB:", e));
   }, []);
 
   return (
@@ -31,7 +31,6 @@ export default function App() {
       <QueryClientProvider client={queryClient}>
         <AuthProvider>
           <RootNavigation />
-          <UpdateAutoChecker />
         </AuthProvider>
       </QueryClientProvider>
     </ErrorBoundary>
