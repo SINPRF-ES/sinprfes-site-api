806db1a2f79c8bc8f70c6e293d3735e77450e8a4
sinprfes <sinprfes@sinprfes.org.br>
2026-02-20 00:00:16 -0300

    Create types.ts

---
+++ mobile/src/navigation/types.ts
@@ -0,0 +1,22 @@
+// mobile/src/navigation/types.ts
+
+export type DrawerParamList = {
+  'Início': undefined;
+  Noticias: undefined;
+  MeusDados: undefined;
+  Filiados: undefined;
+  Publicacoes: undefined;
+  Ressarcimento: undefined;
+  Jogos2026: undefined;
+  Votacao: undefined;
+  Estatuto: undefined;
+  Seguranca: undefined;
+  Atualizacoes: undefined;
+
+  // Condicionais / ocultas
+  Logs: undefined;
+  Repasse: undefined;
+  Relatorios: undefined;
+  NotificacoesPush: undefined;
+  CriarFiliado: undefined;
+};
