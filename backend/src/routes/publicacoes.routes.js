d0ec90df1e808b55114c2f24e049814e36fa25bb
mfavero138 <marcelo.mfb@gmail.com>
2026-03-04 10:24:47 -0300

    Torna publicações públicas e retorna lista em array

--- backend/src/routes/publicacoes.routes.js
+++ backend/src/routes/publicacoes.routes.js
@@ -2,10 +2,8 @@
 const express = require("express");
 const router = express.Router();
 const publicacoesController = require("../controllers/publicacoes.controller");
-const authMiddleware = require("../middlewares/auth");
-
-// Rota GET /api/publicacoes (Protegida pelo login)
-router.get("/", authMiddleware, publicacoesController.listar);
-router.get("/arquivo/:id", authMiddleware, publicacoesController.visualizar);
+// Rota GET /api/publicacoes (Pública para app/site consumir sem token)
+router.get("/", publicacoesController.listar);
+router.get("/arquivo/:id", publicacoesController.visualizar);
 
 module.exports = router;
